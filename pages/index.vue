1dc5572b191efd6fbf8008360fb1ed76bd798295
DanielSarmientoDev <danielsarmientodeveloper@gmail.com>
2021-04-21 09:02:19 -0500

    implement vue3 composition and work graphql api public

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { CATEGORIES } from '~/graphql/queries'
+export default Vue.extend({
+  apollo: {
+    categories: {
+      query: CATEGORIES,
+      update: (data) => {
+        console.log(data)
+      },
+    },
+  },
+})
+</script>
+
+<style></style>
